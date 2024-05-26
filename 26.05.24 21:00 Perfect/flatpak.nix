147d251d4e4c612433ca1bf0a5874f64a37343d9
Rey Z <ryossj@gmail.com>
2024-05-26 21:58:43 +0200

    Add files via upload
    Additional Flatpak - Internet Download Manager included that install automatically. JDownloader and Uget

---
+++ 26.05.24 21:00 Perfect/flatpak.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  # Flatpak und Flatpak-Repositories einbinden
+  environment.systemPackages = with pkgs; [
+    flatpak
+  ];
+
+  # Flatpak-Repositories hinzufügen
+  environment.etc."flatpak/repo.d/flathub.conf".text = ''
+    [remote "flathub"]
+    url=https://dl.flathub.org/repo/
+    gpg-verify=true
+    gpg-verify-summary=true
+    xa.title=Flathub
+    xa.comment=Apps for all Linux devices
+  '';
+
+  # Sicherstellen, dass der Flatpak-Dienst aktiviert ist
+  services.flatpak.enable = true;
+
+  # Sicherstellen, dass das Flatpak-Repository hinzugefügt wird
+  system.activationScripts.flatpak = ''
+    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  '';
+
+  # Flatpak-Installation
+     system.activationScripts.flatpakApps = ''
+     ${pkgs.flatpak}/bin/flatpak install -y flathub org.jdownloader.JDownloader
+     ${pkgs.flatpak}/bin/flatpak install -y flathub com.ugetdm.uGet
+
+   '';
+}
---
+++ 26.05.24 21:00 Perfect/nvidia.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+let
+nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+exec "$@"
+'';
+in
+
+{
+# NVIDIA services
+services.xserver.videoDrivers = [ "nvidia" ];
+
+hardware.opengl = {
+			enable = true;
+			driSupport = true;
+			driSupport32Bit=true;
+			extraPackages = with pkgs;[ vaapiVdpau nvidia-vaapi-driver intel-media-driver]; 
+		};
+
+hardware.nvidia={
+		modesetting.enable = true;
+		powerManagement.enable = true;
+		powerManagement.finegrained = false;
+		open = false;
+		nvidiaSettings = true;
+		package = config.boot.kernelPackages.nvidiaPackages.production;
+	};
+	hardware.nvidia.prime = {
+		offload = { enable = true; enableOffloadCmd=true;
+		};
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+	};
+	programs.git = { enable = true; };
+	time.hardwareClockInLocalTime = true;
+
+}
